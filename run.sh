e9e1348234077452f01c384b2463902d6ce268ae
evaut <39343060+evaut@users.noreply.github.com>
2018-11-25 11:13:01 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 echo 'Start crysadm on'  $(date) >> /tmp/crysadm.txt
 
 sudo pkill redis-server
-sudo pkill python3.4
+sudo pkill python3.5.3
 
 BASE_DIR="$( cd "$( dirname "$0"  )" && pwd  )"                                                             
 ls ${BASE_DIR}/ >> /tmp/error 2>&1
